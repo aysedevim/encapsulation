fb0d1865f7ac41f6cd4b91cb40808241d67e3208
aysedevim <aysedevim@LAPTOP-Q2KIVBU3>
2022-11-27 15:30:24 +0300

    Add project files.

---
+++ kapsulleme/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using kapsulleme;
+
+Musteri m =new Musteri();
+m.ID = 1;
+m.Ad = "Ali";
+m.Soyad = "Kıran";
+m.KartNo = "1111111111";
+Console.WriteLine(m.KartNo);
